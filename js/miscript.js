3911adff4000f5685760c7f75f8ba3ed6b4471d5
Jorge <jorge@geo-thinking.com>
2016-01-28 21:12:07 -0500

    Slider header logos y maqueta terminada sin funcionalidad

---
+++ js/miscript.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+
+var i = 0;
+function cambiarImagen(){
+  var imagenes = [
+    "images/header.png",
+    "images/header2.png",
+    "images/header3.png"
+  ];
+  if(i<imagenes.length-1){
+    i++;
+  } else {
+    i=0;
+  }
+  $('#imagenfondo').fadeOut('slow', function () {
+
+      $('#imagenfondo').css({ 'background-image': 'url("'+imagenes[i]+'")' });
+
+      $('#imagenfondo').fadeIn('slow');
+  });
+}
+setInterval(cambiarImagen, 60000);
+
+});
